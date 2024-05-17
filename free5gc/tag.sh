91f372b678a3e6456072e0c2eb5f71555293bc89
Matysiak Piotr <piotr2.matysiak@orange.com>
2024-05-17 15:53:03 +0200

    add scripts

---
+++ free5gc/tag.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+VERSION=$1
+
+if [[ $VERSION = "" ]]; then
+    echo "Tag version not provided... Exit."
+    echo -e "\n\tUsage:"
+    echo -e "\t\t./tag.sh <VERSION>"
+    echo -e "\n\tExample:"
+    echo -e "\t\t./tag.sh v0.1.0"
+    exit 1
+fi
+
+
+declare -a packages=("amf-nf" "ausf-nf" "free5gc-operator" "mongodb-nf" "nrf-nf" "nssf-nf" "pcf-nf" "smf-nf" "udm-nf" "udr-nf" "upf-nf" "webui-nf")
+
+for package in "${packages[@]}"; do
+    git tag free5gc/$package/$VERSION
+    git push origin free5gc/$package/$VERSION
+done
+
